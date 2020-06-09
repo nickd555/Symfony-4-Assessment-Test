14c2aab495ea7e7626b78aea3ae055ebb9c3b0e2
Nick <nickd555@outlook.com>
2020-06-09 17:24:50 +0200

    Added setters/getters for data

--- AssessmentTest/src/Controller/AppController.php
+++ AssessmentTest/src/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\DataHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,5 +14,11 @@ class AppController extends AbstractController
     public function index()
     {
         return $this->render('app/index.html.twig');
+    }
+
+    public function save() {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $dataHandler = new dataHandler();
     }
 }
--- AssessmentTest/src/Entity/DataHandler.php
+++ AssessmentTest/src/Entity/DataHandler.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\DataHandlerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * @ORM\Entity(repositoryClass=DataHandlerRepository::class)
@@ -55,5 +56,65 @@ class DataHandler
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink($link)
+    {
+        $this->link = $link;
+    }
+
+    public function getDateTime(): ?DateTime
+    {
+        return $this->dateTime;
+    }
+
+    public function setDateTime($DateTime)
+    {
+        $this->DateTime = $DateTime;
+    }
+
+    public function getAdres(): ?string
+    {
+        return $this->id;
+    }
+
+    public function setAdres($Adres)
+    {
+        $this->Adres = $Adres;
+    }
+
+    public function getFloatingNumber(): ?float
+    {
+        return $this->id;
+    }
+
+    public function setFloatingNumber($FloatingNumber)
+    {
+        $this->FloatingNumber = $FloatingNumber;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber($Number)
+    {
+        $this->Number = $Number;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText($Text)
+    {
+        $this->Text = $Text;
     }
 }
